57992382ad28e601bbedec17638260eae94c8236
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-05-30 01:00:53 +0900

    Move terraform state to aws_base

--- aws_apne1/backend.tf
+++ aws_apne1/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket              = "kaigionrails-terraform"
     region              = "ap-northeast-1"
-    key                 = "aws_apne1.tfstate"
+    key                 = "aws_base.tfstate"
     allowed_account_ids = ["861452569180"]
     dynamodb_table      = "kaigionrails-terraform"
   }
