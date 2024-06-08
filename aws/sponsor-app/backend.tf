2c98f12c2d3900e77a6407b7778152240f64e766
Yusuke Nakamura <yusuke1994525@gmail.com>
2024-06-08 22:28:49 +0900

    Rename tfstate key

--- aws/sponsor-app/backend.tf
+++ aws/sponsor-app/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket              = "kaigionrails-terraform"
     region              = "ap-northeast-1"
-    key                 = "aws_sponsor_app.tfstate"
+    key                 = "aws_sponsor-app.tfstate"
     allowed_account_ids = ["861452569180"]
     dynamodb_table      = "kaigionrails-terraform"
   }
