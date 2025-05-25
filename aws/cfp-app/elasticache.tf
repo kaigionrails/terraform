25b2dde7fba93ada5b71a2be9fd19a015efd3a8e
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-05-25 23:52:36 +0900

    Cannot use serverless vakey, create single-mode valkey for cfp-app

--- aws/cfp-app/elasticache.tf
+++ aws/cfp-app/elasticache.tf
@@ -19,13 +19,30 @@ resource "aws_vpc_security_group_ingress_rule" "cfp_app_cache_inbound_from_worke
   referenced_security_group_id = aws_security_group.cfp_app_worker.id
 }
 
-resource "aws_elasticache_serverless_cache" "cfp_app" {
+# serverless valkey uses cluster mode but sidekiq not support cluster mode
+# resource "aws_elasticache_serverless_cache" "cfp_app" {
+#   engine               = "valkey"
+#   name                 = "cfp-app"
+#   major_engine_version = 7
+#   security_group_ids   = [aws_security_group.cfp_app_cache.id]
+#   subnet_ids = [
+#     data.aws_subnet.kaigionrails_apne1_c_private.id,
+#     data.aws_subnet.kaigionrails_apne1_d_private.id,
+#   ]
+# }
+
+data "aws_elasticache_subnet_group" "kaigionrails_apne1_private" {
+  name = "kaigionrails-apne1-private"
+}
+
+resource "aws_elasticache_replication_group" "cfp_app" {
+  replication_group_id = "cfp-app-singlemode"
+  description          = "cache for cfp-app, single mode"
   engine               = "valkey"
-  name                 = "cfp-app"
-  major_engine_version = 7
+  engine_version       = "7.2"
+  cluster_mode         = "disabled"
+  node_type            = "cache.t2.micro"
+  apply_immediately    = true
   security_group_ids   = [aws_security_group.cfp_app_cache.id]
-  subnet_ids = [
-    data.aws_subnet.kaigionrails_apne1_c_private.id,
-    data.aws_subnet.kaigionrails_apne1_d_private.id,
-  ]
+  subnet_group_name    = data.aws_elasticache_subnet_group.kaigionrails_apne1_private.name
 }
