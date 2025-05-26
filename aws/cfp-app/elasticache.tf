3d7cc695e3cc95a085270b105e74d79d426c6b7b
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-05-26 22:12:36 +0900

    Remove static resource
    Hikkoshi is interrupted

--- aws/cfp-app/elasticache.tf
+++ aws/cfp-app/elasticache.tf
@@ -35,14 +35,15 @@ data "aws_elasticache_subnet_group" "kaigionrails_apne1_private" {
   name = "kaigionrails-apne1-private"
 }
 
-resource "aws_elasticache_replication_group" "cfp_app" {
-  replication_group_id = "cfp-app-singlemode"
-  description          = "cache for cfp-app, single mode"
-  engine               = "valkey"
-  engine_version       = "7.2"
-  cluster_mode         = "disabled"
-  node_type            = "cache.t2.micro"
-  apply_immediately    = true
-  security_group_ids   = [aws_security_group.cfp_app_cache.id]
-  subnet_group_name    = data.aws_elasticache_subnet_group.kaigionrails_apne1_private.name
-}
+# Hikkoshi is interrupted
+# resource "aws_elasticache_replication_group" "cfp_app" {
+#   replication_group_id = "cfp-app-singlemode"
+#   description          = "cache for cfp-app, single mode"
+#   engine               = "valkey"
+#   engine_version       = "7.2"
+#   cluster_mode         = "disabled"
+#   node_type            = "cache.t2.micro"
+#   apply_immediately    = true
+#   security_group_ids   = [aws_security_group.cfp_app_cache.id]
+#   subnet_group_name    = data.aws_elasticache_subnet_group.kaigionrails_apne1_private.name
+# }
