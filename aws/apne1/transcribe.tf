dafcb887efae84bc5ec631a7f53fa49b893a16a9
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-23 02:36:12 +0900

    Import pre-created transcribe language models

--- aws/apne1/transcribe.tf
+++ aws/apne1/transcribe.tf
@@ -1,10 +1,19 @@
-# Takes very long time
-# resource "aws_transcribe_language_model" "kaigionrails_ja" {
-#   model_name      = "kaigionrails-ja"
-#   base_model_name = "WideBand"
-#   language_code   = "ja-JP"
-#   input_data_config {
-#     data_access_role_arn = aws_iam_role.kaigionrails_archive_bucket_read.arn
-#     s3_uri               = "s3://${aws_s3_bucket.kaigionrails_archive.id}/2024/transcription/ja/"
-#   }
-# }
+resource "aws_transcribe_language_model" "kaigionrails_ja" {
+  model_name      = "kaigionrails-ja"
+  base_model_name = "WideBand"
+  language_code   = "ja-JP"
+  input_data_config {
+    data_access_role_arn = aws_iam_role.kaigionrails_archive_bucket_read.arn
+    s3_uri               = "s3://${aws_s3_bucket.kaigionrails_archive.id}/2024/transcription/ja/"
+  }
+}
+
+resource "aws_transcribe_language_model" "kaigionrails_en" {
+  model_name      = "kaigionrails-en"
+  base_model_name = "WideBand"
+  language_code   = "en-US"
+  input_data_config {
+    data_access_role_arn = aws_iam_role.kaigionrails_archive_bucket_read.arn
+    s3_uri               = "s3://${aws_s3_bucket.kaigionrails_archive.id}/2024/transcription/en/"
+  }
+}
