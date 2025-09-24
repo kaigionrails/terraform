8adccc41bfd63f8113da8120c2e469d9205b899e
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-24 17:54:25 +0900

    Create log bucket

--- aws/apne1/s3.tf
+++ aws/apne1/s3.tf
@@ -9,3 +9,41 @@ resource "aws_s3_bucket_ownership_controls" "kaigionrails_archive" {
     object_ownership = "BucketOwnerEnforced"
   }
 }
+
+resource "aws_s3_bucket" "kaigionrails_logs" {
+  bucket = "kaigionrails-logs"
+}
+
+resource "aws_s3_bucket_ownership_controls" "kaigionrails_logs" {
+  bucket = aws_s3_bucket.kaigionrails_logs.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+data "aws_iam_policy_document" "kaigionrails_logs_logging_policy" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.kaigionrails_logs.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["logdelivery.elasticloadbalancing.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:elasticloadbalancing:*:*:loadbalancer/*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "kaigionrails_logs_logging_policy" {
+  bucket = aws_s3_bucket.kaigionrails_logs.id
+  policy = data.aws_iam_policy_document.kaigionrails_logs_logging_policy.json
+}
