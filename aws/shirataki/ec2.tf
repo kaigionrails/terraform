4cf046f6fdad4e7a2e386fa96e5cd40f07642ae6
Yusuke Nakamura <yusuke1994525@gmail.com>
2025-09-25 06:44:44 +0900

    Issue cert and make secure shirataki-staging

--- aws/shirataki/ec2.tf
+++ aws/shirataki/ec2.tf
@@ -125,10 +125,33 @@ resource "aws_lb_target_group" "shirataki_staging" {
   }
 }
 
+resource "aws_acm_certificate" "shirataki_staging" {
+  domain_name       = "shirataki-staging.kaigionrails.org"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb_listener" "shirataki_staging_http" {
   load_balancer_arn = aws_lb.shirataki_staging.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.shirataki_staging.arn
+  }
+}
+
+
+resource "aws_lb_listener" "shirataki_staging_https" {
+  load_balancer_arn = aws_lb.shirataki_staging.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.shirataki_staging.arn
 
   default_action {
     type             = "forward"
